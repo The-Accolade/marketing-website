35ee00935d489baa3cc7a74c6da66f1d547c8240
kawairish <kawairishpurity@gmail.com>
2023-12-11 11:59:13 +0300

    completed landing page

--- src/components/Home/Partners.jsx
+++ src/components/Home/Partners.jsx
@@ -1,58 +1,92 @@
-import { faAlgolia, faGithub, faGoogle, faIntercom, faSlack, faUsps, faWix } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+	faAlgolia,
+	faGithub,
+	faGoogle,
+	faIntercom,
+	faSlack,
+	faUsps,
+	faWix,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Slider from "react-slick";
 
-
-
 const Partners = () => {
+	const settings = {
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		speed: 500,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		centerMode: true,
+		centerPadding: "200px",
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+				},
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+			// You can unslick at a given breakpoint now by adding:
+			// settings: "unslick"
+			// instead of a settings object
+		],
+	};
 
-  const settings = {
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  };
-
-  return (
-    <Slider {...settings} className="w-full flex justify-between bg-shadeOne py-[30px]">
-      <FontAwesomeIcon className='h-[30px] text-blackShade' icon={faAlgolia} />
-      <FontAwesomeIcon className='h-[30px] text-blackShade' icon={faSlack}/>
-      <FontAwesomeIcon className='h-[30px] text-blackShade' icon={faGoogle}/>
-      <FontAwesomeIcon className='h-[30px] text-blackShade' icon={faGithub}/>
-      <FontAwesomeIcon className='h-[30px] text-blackShade' icon={faIntercom}/>
-      <FontAwesomeIcon className='h-[30px] text-blackShade' icon={faWix}/>
-      <FontAwesomeIcon className='h-[30px] text-blackShade' icon={faUsps}/>
-      <FontAwesomeIcon className='h-[30px] text-blackShade' icon={faUsps}/>
-    </Slider>
-  );
-}
+	return (
+		<Slider
+			{...settings}
+			className="w-full flex justify-between  py-[50px] h-[30vh] max-md:h-full"
+		>
+			<FontAwesomeIcon
+				className="h-[35px] text-blackShade bg-shadeOne opacity-80 py-5"
+				icon={faAlgolia}
+			/>
+			<FontAwesomeIcon
+				className="h-[35px] text-blackShade bg-shadeOne opacity-80 py-5"
+				icon={faSlack}
+			/>
+			<FontAwesomeIcon
+				className="h-[35px] text-blackShade bg-shadeOne opacity-80 py-5"
+				icon={faGoogle}
+			/>
+			<FontAwesomeIcon
+				className="h-[35px] text-blackShade bg-shadeOne opacity-80 py-5"
+				icon={faGithub}
+			/>
+			<FontAwesomeIcon
+				className="h-[35px] text-blackShade bg-shadeOne opacity-80 py-5"
+				icon={faIntercom}
+			/>
+			<FontAwesomeIcon
+				className="h-[35px] text-blackShade bg-shadeOne opacity-80 py-5"
+				icon={faWix}
+			/>
+			<FontAwesomeIcon
+				className="h-[35px] text-blackShade bg-shadeOne opacity-80 py-5"
+				icon={faUsps}
+			/>
+			<FontAwesomeIcon
+				className="h-[35px] text-blackShade bg-shadeOne opacity-80 py-5"
+				icon={faUsps}
+			/>
+		</Slider>
+	);
+};
 
 export default Partners;
--- src/components/Home/WhyChooseUs.jsx
+++ src/components/Home/WhyChooseUs.jsx
@@ -2,28 +2,83 @@ import ProgressBar from "@ramonak/react-progress-bar";
 
 const WhyChooseUs = () => {
   return (
-    <section className='py-[100px] px-[200px] bg-whiteShade flex items-center  justify-between'>
-        <div>
-            <iframe className="h-[300px]" src="https://www.youtube.com/embed/XZ5NaZ2Ucdo?si=-N-pcfHuurcMQ9rs" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        </div>
-        <div className="w-2/4">
-            <h4 className="text-primary font-semibold text-[14px] uppercase">Why choose us</h4>
-            <h3 className="text-blackShade font-bold font-bricolage text-[32px] leading-9">A right choice that makes the difference to others</h3>
-            <p className="py-2 font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sapien eget mi proin sed libero enim. Tortor condimentum lacinia quis vel eros donec.</p>
-            <div>
-                <h5>Business Planning</h5>
-                <ProgressBar 
-                    className="wrapper"
-                    barContainerClassName="container"
-                    completedClassName="barCompleted"
-                    labelClassName="label"
-                    transitionDuration="1s"
-                    transitionTimingFunction="linear" 
-                    completed="60" />
-            </div>
-        </div>
-    </section>
-  )
+		<section className="py-[100px] px-[200px] mx-auto max-lg:px-[50px] bg-whiteShade flex items-center  justify-between max-lg:gap-5 max-md:flex-col-reverse max-md:gap-8">
+			<div>
+				<iframe
+					className="h-[400px] max-md:w-[500px] max-md:mx-auto"
+					src="https://www.youtube.com/embed/XZ5NaZ2Ucdo?si=-N-pcfHuurcMQ9rs"
+					title="YouTube video player"
+					frameBorder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+					allowfullscreen
+				></iframe>
+			</div>
+			<div className="w-2/4 max-md:w-[90%] max-md:mx-auto">
+				<h4 className="text-primary font-semibold text-[14px] uppercase">
+					Why choose us
+				</h4>
+				<h3 className="text-blackShade font-bold font-bricolage text-[32px] leading-9">
+					A right choice that makes the difference to others
+				</h3>
+				<p className="py-2 font-light">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+					eiusmod tempor incididunt ut labore et dolore magna aliqua. Sapien
+					eget mi proin sed libero enim. Tortor condimentum lacinia quis vel
+					eros donec.
+				</p>
+				<div>
+					<div>
+						<h5>Business Planning</h5>
+						<ProgressBar
+							className="wrapper"
+							barContainerClassName="container"
+							completedClassName="barCompleted w-[60%]"
+							labelClassName="label"
+							transitionDuration="1s"
+							transitionTimingFunction="linear"
+							completed="60"
+						/>
+					</div>
+					<div className="my-4">
+						<h5>Financial Advices</h5>
+						<ProgressBar
+							className="wrapper"
+							barContainerClassName="container"
+							completedClassName="barCompleted w-[80%]"
+							labelClassName="label"
+							transitionDuration="1s"
+							transitionTimingFunction="linear"
+							completed="80"
+						/>
+					</div>
+					<div>
+						<h5>Investment Strategy</h5>
+						<ProgressBar
+							className="wrapper"
+							barContainerClassName="container"
+							completedClassName="barCompleted w-[70%]"
+							labelClassName="label"
+							transitionDuration="1s"
+							transitionTimingFunction="linear"
+							completed="70"
+						/>
+					</div>
+					<div className="mt-4">
+						<h5>Business Security</h5>
+						<ProgressBar
+							className="wrapper"
+							barContainerClassName="container"
+							completedClassName="barCompleted w-[90%]"
+							labelClassName="label"
+							transitionDuration="1s"
+							transitionTimingFunction="linear"
+							completed="90"
+						/>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default WhyChooseUs
